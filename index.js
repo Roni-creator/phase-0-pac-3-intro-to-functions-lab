bdf0f793dce9658b81c776f4bfcb6f37a4d2dc3e
veronicah <mbuviveronicah3@gmail.com>
2023-03-11 18:45:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {}
+function shout(string) {
+    return string();
+  }
+  "Hello!".toUpperCase();
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {}
+function whisper(string) {
+    return string();
+  }
+  "Hello!".toLowerCase();
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logshout(string){
+    console.log('HELLO'.toUpperCase());
+  }
+function logWhisper(string) {
+    console.log('Hello'.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+    if(string==='HELLO')
+return"Ican\'t hear you!
+else if(string==="HELLO")
+return "YES INDEED!"
+else(string==="let's have dinner together!")
+return "I would love to!"}
